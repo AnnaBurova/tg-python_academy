ec5a69acfda7fc40db33bbd3e19600a07cb3bf30
AnnaBurova <50639099+AnnaBurova@users.noreply.github.com>
2024-11-21 18:58:00 +0300

    Create colorama.py

---
+++ code/colorama.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov  2 02:14:51 2024
+
+@author: KoreAnna
+"""
+
+from colorama import Fore, Back, Style
+
+# Инициализация (для Windows; на других системах можно не вызывать)
+from colorama import init
+init()
+
+print(Style.DIM)
+print(Fore.BLACK + "Это обычный текст")
+print(Fore.RED + "Это обычный текст")
+print(Fore.GREEN + "Это обычный текст")
+print(Fore.YELLOW + "Это обычный текст")
+print(Fore.BLUE + "Это обычный текст")
+print(Fore.MAGENTA + "Это обычный текст")
+print(Fore.CYAN + "Это обычный текст")
+print(Fore.WHITE + "Это обычный текст")
+print(Back.BLACK + "Это обычный текст")
+print(Back.RED + "Это обычный текст")
+print(Back.GREEN + "Это обычный текст")
+print(Back.YELLOW + "Это обычный текст")
+print(Back.BLUE + "Это обычный текст")
+print(Back.MAGENTA + "Это обычный текст")
+print(Back.CYAN + "Это обычный текст")
+print(Back.WHITE + "Это обычный текст")
+
+print()
+print(Style.RESET_ALL + "Сброс")
+
+print(Style.NORMAL)
+print(Fore.BLACK + "Это обычный текст")
+print(Fore.RED + "Это обычный текст")
+print(Fore.YELLOW + "Это обычный текст")
+print(Fore.BLUE + "Это обычный текст")
+print(Fore.MAGENTA + "Это обычный текст")
+print(Fore.CYAN + "Это обычный текст")
+print(Fore.WHITE + "Это обычный текст")
+print(Fore.GREEN + "Это обычный текст")
+print(Back.BLACK + "Это обычный текст")
+print(Back.RED + "Это обычный текст")
+print(Back.GREEN + "Это обычный текст")
+print(Back.YELLOW + "Это обычный текст")
+print(Back.BLUE + "Это обычный текст")
+print(Back.MAGENTA + "Это обычный текст")
+print(Back.CYAN + "Это обычный текст")
+print(Back.WHITE + "Это обычный текст")
+
+print()
+print(Style.RESET_ALL + "Сброс")
+
+print(Style.BRIGHT)
+print(Fore.RED + "Это обычный текст")
+print(Fore.YELLOW + "Это обычный текст")
+print(Fore.BLUE + "Это обычный текст")
+print(Fore.MAGENTA + "Это обычный текст")
+print(Fore.CYAN + "Это обычный текст")
+print(Fore.GREEN + "Это обычный текст")
+print(Fore.BLACK + "Это обычный текст")
+print(Fore.WHITE + "Это обычный текст")
+print(Back.BLACK + "Это обычный текст")
+print(Back.RED + "Это обычный текст")
+print(Back.GREEN + "Это обычный текст")
+print(Back.YELLOW + "Это обычный текст")
+print(Back.BLUE + "Это обычный текст")
+print(Back.MAGENTA + "Это обычный текст")
+print(Back.CYAN + "Это обычный текст")
+print(Back.WHITE + "Это обычный текст")
